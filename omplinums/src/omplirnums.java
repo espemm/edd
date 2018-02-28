409d42b8cbaabd661c0807ad19d0e0de7f96c952
espemm <espemm@gmail.com>
2018-02-28 23:37:36 +0100

    Add files via upload

---
+++ omplinums/src/omplirnums.java
@@ -0,0 +1,27 @@
+
+public class omplirnums {
+
+	public static void main(String[] args) {
+		int n=5;
+		int[] taula = new int[n];
+		taula=omplir(n);
+		int suma = sumar(taula);
+		System.out.println ("La suma es: "+ suma);
+		System.out.println ("jesus");
+	
+	}
+	private static int[] omplir(int n) {
+		int[] taula = new int [n];
+		for (int i=0; i<n; i++)
+			taula[i] = i * 10;
+		return taula;
+	}
+	private static int sumar(int[] taula){
+		int suma = 0;
+		int n= taula.length;
+		for (int i=0; i<n; i++)
+			suma =  suma + taula[i];
+		return suma;
+	}
+
+}
