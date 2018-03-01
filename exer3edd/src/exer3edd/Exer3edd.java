938bbf9e3ff08d38aa83245120bd5e8e4ab72d98
monyert <monyert@monyerti5>
2018-03-01 12:52:18 +0100

    Jordi_Sales

--- exer3edd/src/exer3edd/Exer3edd.java
+++ exer3edd/src/exer3edd/Exer3edd.java
@@ -15,7 +15,7 @@ public class Exer3edd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Jordi Sales Aleixandre");
+         System.out.println("Jordi Sales Aleixandre");
     }
     
 }
