cf6c9ff0b7a9102592803f3bf5c8d5879edff057
Alex Lopez <alexPC>
2018-03-01 12:37:07 +0100

    Modificat per Alex López Meló

---
+++ edd/src/edd/Edd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd;
+
+/**
+ *
+ * @author Aren
+ */
+public class Edd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Alex López Meló");
+    }
+    
+}
